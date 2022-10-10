e25e42015f27f1e3786c3b1a500e3e9f831f662d
Nastia Kartseva <danetzachem@gmail.com>
2022-10-10 14:21:37 +0300

    Implemented Item card component with styles

---
+++ src/components/ItemCard.jsx
@@ -0,0 +1,18 @@
+import styles from '../styles/ItemCard.module.css';
+
+const ItemCard = ({ data }) => {
+  const { name, author, picture, price, inStock } = data;
+  return (
+    <div className={styles.card}>
+      <img className={styles.image} src={picture} alt="Обложка книги" />
+      <h2 className={styles.title}>{name}</h2>
+      <span>{author}</span>
+      <span className={styles.price}>{price}</span>
+      <button className={inStock ? styles.addBtn : styles.disabledBtn}>
+        {inStock ? 'В корзину' : 'Нет в наличии'}
+      </button>
+    </div>
+  );
+};
+
+export default ItemCard;
