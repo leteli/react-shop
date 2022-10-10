b9d2edf38c0fc5854f2253e8ba07e5c4c7c7912b
Nastia Kartseva <danetzachem@gmail.com>
2022-10-10 14:10:15 +0300

    Updated About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,13 @@
 import Layout from './Layout.jsx';
+import layout from '../styles/Layout.module.css';
+import styles from '../styles/About.module.css';
 
 const About = () => {
   return (
     <Layout>
-      
+      <h1 className={layout.title}>О магазине</h1>
+      <p className={styles.text}><span className={styles.coloredText}>Книги</span> - независимый книжный магазин в центре Санкт-Петербурга.<br/>Мы предлагаем широкий ассортимент гуманитарной non-fiction литературы, поэзии, художественной литературы</p>
+      <p className={styles.text}>Предлагаем вам сделать заказ в нашем удобном <span className={styles.coloredText}>онлайн-магазине</span>. Мы можем доставить ваш заказ по указанному адресу, также возможен самовывоз.</p>
     </Layout>
   );
 };
