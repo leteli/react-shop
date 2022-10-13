4555bc2da11db965428e86db0d89eb055793cab3
Nastia Kartseva <danetzachem@gmail.com>
2022-10-13 15:46:34 +0300

    Added utility functions

--- src/utils.js
+++ src/utils.js
@@ -3,4 +3,14 @@ const priceToNumber = (price) => {
   return Number(count);
 };
 
+export const getTotalAmount = (state) => {
+  return state.cart.length === 0 ? 0 : state.cart
+    .reduce((acc, item) => acc + item.quantity, 0);
+};
+
+export const getTotalPrice = (state) => {
+  return state.cart.length === 0 ? 0 : state.cart
+    .reduce((acc, item) => acc + item.totalPrice, 0);
+};
+
 export default priceToNumber;
