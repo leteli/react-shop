a82b39faa2f841e9bf9f0e778e229846dfa178dc
Nastia Kartseva <danetzachem@gmail.com>
2022-10-14 13:12:00 +0300

    Added utility functions

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,3 @@
-const priceToNumber = (price) => {
-  const [count, ] = price.split(' ');
-  return Number(count);
-};
-
 export const getTotalAmount = (state) => {
   return state.cart.length === 0 ? 0 : state.cart
     .reduce((acc, item) => acc + item.quantity, 0);
@@ -13,4 +8,42 @@ export const getTotalPrice = (state) => {
     .reduce((acc, item) => acc + item.totalPrice, 0);
 };
 
-export default priceToNumber;
+// const validate = (data) => {
+//   for (const key in values) {
+//       const { value } = values[key];
+//   }
+// }
+
+const validate = (formData) => {
+  const entries = formData.entries();
+  const validatedData = {};
+  for (const [name, rawValue] of entries) {
+    const value = rawValue.trim();
+    if (value === '') {
+      validatedData[name] = { isValid: false, message: 'Поле не должно быть пустым' };
+    } else if (name === 'description'  && value.length > 1000) {
+      validatedData[name] = { isValid: false, message: 'Не более 600 символов' };
+    } else if (name === 'description') {
+      validatedData[name] = { isValid: true, value };
+    } else if (value.length > 60) {
+      validatedData[name] = { isValid: false, message: 'Не более 30 символов' };
+    } else {
+      validatedData[name] = { isValid: true, value };
+    }
+  }
+  return validatedData;
+};
+
+export const getUpdatedData = (validatedData) => {
+  const { name, author, price, inStock, description } = validatedData;
+  return {
+    name: name.value,
+    author: author.value,
+    price: price.value,
+    inStock: inStock.value,
+    description: description.value,
+  }
+};
+
+
+export default validate;
