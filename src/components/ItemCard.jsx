c7b9ce005b1e0defaab15ffbe3be940f68385e3b
Nastia Kartseva <danetzachem@gmail.com>
2022-10-14 13:10:40 +0300

    Added logic to ItemCard

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,20 +1,29 @@
 import { Link } from 'react-router-dom';
-// import { useDispatch } from 'react-redux';
-// import { addedToCart } from '../store/actions/cartActions.js';
+import { useSelector } from 'react-redux';
+import useLoginModalContext from '../hooks/useLoginModalContext.js';
 import AddButton from './AddButton.jsx';
 import styles from '../styles/ItemCard.module.css';
 
 const ItemCard = ({ data }) => {
   const { id, name, author, picture, price, inStock } = data;
-  // const dispatch = useDispatch();
-  // const handleAdd = (data) => () => dispatch(addedToCart(data));
+  const { isLoggedIn, currentUser } = useLoginModalContext();
+  const prevCount = useSelector((state) => {
+    const cartProduct = state.cart.find((product) => product.id === id);
+    return cartProduct ? cartProduct.quantity : 0;
+  });
   return (
-    <div className={styles.card}>
+    <div key={id} className={styles.card}>
       <img className={styles.image} src={picture} alt="Обложка книги" />
       <Link to={`/products/${id}`}><h2 className={styles.title}>{name}</h2></Link>
       <span>{author}</span>
-      <span className={styles.price}>{price}</span>
-      <AddButton data={data} style={inStock !== 0 ? styles.addBtn : styles.disabledBtn} />
+      <span className={styles.price}>{price} ₽</span>
+      { currentUser === 'user' ? (
+      <AddButton
+        inputCount={1}
+        prevCount={prevCount}
+        data={data}
+        style={inStock !== 0 ? styles.addBtn : styles.disabledBtn} />
+      ) : isLoggedIn ? null : <div className={styles.needLogin}>Войдите, чтобы добавить в корзину</div>}
     </div>
   );
 };
