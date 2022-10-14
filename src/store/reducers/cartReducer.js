d80c21571c9596d6c10eb361b8dac16d617a8414
Nastia Kartseva <danetzachem@gmail.com>
2022-10-14 13:14:08 +0300

    Implemented reducers

--- src/store/reducers/cartReducer.js
+++ src/store/reducers/cartReducer.js
@@ -1,26 +1,14 @@
-import priceToNumber from '../../utils.js';
-
 const cartReducer = (state = [], action) => {
   switch (action.type) {
     case 'cartFetched': {
-      return action.payload;
+      return [ ...action.payload];
     }
     case 'addedToCart': {
-      const { data, newCountInCart } = action.payload;
-      const { id, name, price } = data;
-      const itemInCart = state.find((item) => item.id === id);
-      const restItems = state.filter((item) => item.id !== id);
-      const newQuantity = itemInCart ? itemInCart.quantity + newCountInCart : newCountInCart;
-      const newItemInCart = {
-        id,
-        name,
-        price,
-        quantity: newQuantity,
-        totalPrice: newQuantity * priceToNumber(price),
-      };
-      return [...restItems, newItemInCart];
+      const newProduct = action.payload;
+      const restProducts = state.filter((item) => item.id !== newProduct.id);
+      return [...restProducts, newProduct];
     }
-    case 'deletedFromCart': {
+    case 'removedFromCart': {
       const id = action.payload;
       const restData = state.filter((item) => item.id !== id);
       return restData;
--- src/store/reducers/productsReducer.js
+++ src/store/reducers/productsReducer.js
@@ -1,16 +1,9 @@
 const productsReducer = (state = [], action) => {
   switch (action.type) {
     case 'productsFetched': {
-      return action.payload;
+      return [...action.payload];
     }
-    case 'changedAmount': {
-      const { data, prevCountInCart, newCountInCart } = action.payload;
-      const diff = newCountInCart - prevCountInCart;
-      const changedItem = { ...data, inStock: data.inStock - diff };
-      const restItems = state.filter((item) => item.id !== data.id);
-      return [...restItems, changedItem];
-    }
-    case 'productEdited': { // мб изменить - передавать только нужные данные и id
+    case 'productUpdated': {
       const newData = action.payload;
       const restData = state.filter((p) => p.id !== newData.id);
       return [...restData, newData];
