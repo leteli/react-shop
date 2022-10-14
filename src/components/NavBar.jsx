e18e0a659014ac6f31d1e53dc900c371bbde51c1
Nastia Kartseva <danetzachem@gmail.com>
2022-10-14 13:15:37 +0300

    Updated logic in NavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,39 @@
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import priceToNumber from '../utils.js';
+import useLoginModalContext from '../hooks/useLoginModalContext.js';
+import { getTotalAmount } from '../utils.js';
+import { getTotalPrice } from '../utils.js';
 import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
-  const productsAmount = useSelector((state) => state.cart.length);
-  const totalPrice = useSelector((state) => {
-    return state.cart.length === 0 ? 0 : state.cart
-      .reduce((acc, item) => acc + priceToNumber(item.price), 0);
-  });
-  
+  const totalAmount = useSelector(getTotalAmount);
+  const totalPrice = useSelector(getTotalPrice);
+  const { toggleModal, isLoggedIn, changeLoginStatus, currentUser, setUser } = useLoginModalContext();
+  const logOut = () => {
+    changeLoginStatus();
+    setUser(null);
+  }
+
   return (
     <nav className={styles.navbar}>
       <NavLink className={styles.home} to="/">Главная</NavLink>
       <NavLink to="/about">О магазине</NavLink>
       <div className={styles.rightNav}>
+        { currentUser === 'user' && (
         <NavLink to="/cart" className={styles.cart}>
           <img src="/assets/cart.svg" alt="Иконка корзины" />
           <div className={styles.cartInfo}>
             <span className={styles.cartTitle}>Корзина</span>
-            <span>Товаров: {productsAmount}</span>
+            <span>Товаров: {totalAmount}</span>
             <span>{totalPrice} ₽</span>
           </div>
         </NavLink>
-        <button className={styles.button}>Войти</button>
+        )}
+        {isLoggedIn ? (
+          <button onClick={logOut} className={styles.button}>Выход</button>
+        ) : (
+        <button onClick={toggleModal} className={styles.button}>Войти</button>
+        )}
       </div>
     </nav>
   )
