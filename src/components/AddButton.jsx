41269a5d2079a98197cfb5eb5dac0c2b32fea735
Nastia Kartseva <danetzachem@gmail.com>
2022-10-14 13:12:37 +0300

    Implemented add to cart logic

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,11 +1,19 @@
 import { useDispatch } from 'react-redux';
-import { addedToCart } from '../store/actions/cartActions.js';
-import styles from '../styles/ItemCard.module.css';
+import { middlewareAddToCart } from '../middlewares/cart.js';
+import middlewareChangedAmount from '../middlewares/products.js';
+// import { addToCart } from '../api/routes.js';
+// import { addedToCart } from '../store/actions/cartActions.js';
+// import { changedAmount } from '../store/actions/productsActions.js';
+// import styles from '../styles/ItemCard.module.css';
 
-const AddButton = ({ data, style }) => {;
+const AddButton = ({ data, setCurrentData, inputCount, style }) => {;
   const { inStock } = data;
   const dispatch = useDispatch();
-  const handleAdd = (data) => () => dispatch(addedToCart(data));
+  const handleAdd = (data) => () => {
+    dispatch(middlewareAddToCart(data, inputCount));
+    dispatch(middlewareChangedAmount(data.id, inputCount));
+  };
+
   return (
     <button
       onClick={handleAdd(data)}
