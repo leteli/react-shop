cbe0c1cb1819c7d6a62d4a000551c81cd22a1108
Nastia Kartseva <danetzachem@gmail.com>
2022-10-18 17:31:44 +0300

    Migrated index file to TS, added types for redux

---
+++ client/src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './store/reducers/rootReducer';
+// import StoreProvider from './components/StoreProvider';
+import LoginProvider from './components/LoginProvider';
+import App from './components/App';
+import './styles/index.css';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+  <LoginProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </LoginProvider>
+);
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof rootReducer>;
