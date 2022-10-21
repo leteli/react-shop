690edd1c8b60f2351ae5ac9f94627aaca2174b32
Nastia Kartseva <danetzachem@gmail.com>
2022-10-21 21:32:10 +0300

    Added tests for utils

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -6,10 +6,9 @@ export const getTotalAmount = (cart: ICartProductData[]) => {
 };
 
 export const getTotalPrice = (cart: ICartProductData[]) => cart
-    .reduce((acc: number, item) => acc + item.totalPrice, 0);
+  .reduce((acc: number, item) => acc + item.totalPrice, 0);
 
-const validate = (formData: FormData): ValidatedForm => {
-  const entries = formData.entries();
+const validate = (entries: Array<[string, string]>): ValidatedForm => {
   const validatedData: ValidForm | InvalidForm = {};
   for (const [name, rawValue] of entries) {
     let value = rawValue;
@@ -18,11 +17,11 @@ const validate = (formData: FormData): ValidatedForm => {
       if (value === '') {
         validatedData[name] = { isValid: false, error: 'Поле не должно быть пустым' };
       } else if (name === 'description'  && value.length > 1000) {
-        validatedData[name] = { isValid: false, error: 'Не более 600 символов' };
+        validatedData[name] = { isValid: false, error: 'Не более 1000 символов' };
       } else if (name === 'description') {
         validatedData[name] = { isValid: true, value, error: '' };
       } else if (value.length > 60) {
-        validatedData[name] = { isValid: false, error: 'Не более 30 символов' };
+        validatedData[name] = { isValid: false, error: 'Не более 60 символов' };
       } else {
         validatedData[name] = { isValid: true, value,  error: '' };
       }
@@ -31,7 +30,7 @@ const validate = (formData: FormData): ValidatedForm => {
   return validatedData;
 };
 
-export const getUpdatedData = (validatedData: ValidatedForm): IUpdatedData => {
+export const getUpdatedData = (validatedData: ValidForm): IUpdatedData => {
   const { name, author, price, inStock, description } = validatedData as ValidForm;
   return {
     name: name.value,
